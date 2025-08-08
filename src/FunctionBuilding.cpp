d2ea24755001f792541025a8c9f89bc09e2a8ec4
localhost <localhost@localhost>
2025-08-08 12:01:00 +0800

    初步实现仙器炼丹

--- src/FunctionBuilding.cpp
+++ src/FunctionBuilding.cpp
@@ -10,6 +10,18 @@ void MyFrame::xianqi_liandan(WXBTNEVT&){
   auto btn1 = new wxButton(panel,wxID_ANY,wxT("10仙器法力+2普通丹\n1碧丹"));
   MYSHOPBUTTON(btn1, MyDarkOrange);
   btn1 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&){
+    if(!XianQiFaLiReader.high(10)){
+      MYMESSAGE("仙器法力不足");
+      return;
+    }
+    if(!Xian::Dan::PuTong.high(2)){
+      MYMESSAGE("普通丹不足");
+      return;
+    }
+    XianQiFaLiReader.minusnum(10);
+    Xian::Dan::PuTong.minusnum(2);
+    Xian::Dan::Bi.addnum(1);
+    MYMESSAGE("炼制成功");
   });
   grid -> Add(btn1, FLAG_CENTER);
   
