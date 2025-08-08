15708a921be2b070919c3bd18bcd6dbaa28e99ec
localhost <localhost@localhost>
2025-08-08 11:30:28 +0800

    继续实现仙器炼丹

--- src/FunctionBuilding.cpp
+++ src/FunctionBuilding.cpp
@@ -5,6 +5,16 @@ void MyFrame::xianqi_liandan(WXBTNEVT&){
 
   MYTITLE("仙器炼丹");
 
+  auto grid = new wxGridSizer(3,3,4,4);
+
+  auto btn1 = new wxButton(panel,wxID_ANY,wxT("10仙器法力+2普通丹\n1碧丹"));
+  MYSHOPBUTTON(btn1, MyDarkOrange);
+  btn1 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&){
+  });
+  grid -> Add(btn1, FLAG_CENTER);
+  
+  vbox -> Add(grid, FLAG_CENTER);
+
   MYADDSPACER();
 
   MYLAST(&MyFrame::xianqi);
