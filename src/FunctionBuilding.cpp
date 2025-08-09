3116308b53310f456f47bab1a112e942228968a7
localhost <localhost@localhost>
2025-08-09 12:27:06 +0800

    继续实现仙籍炼丹

--- src/FunctionBuilding.cpp
+++ src/FunctionBuilding.cpp
@@ -7,6 +7,12 @@ void MyFrame::xianji_liandan(WXBTNEVT&){
 
   auto grid = new wxGridSizer(2,3,4,4);
 
+  auto btn1 = new wxButton(panel,wxID_ANY,wxT("10仙籍+2银丹\n1金丹"));
+  MYSHOPBUTTON(btn1, MyOrange);
+  btn1 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&){
+  });
+  grid -> Add(btn1, FLAG_CENTER);
+
   vbox -> Add(grid, FLAG_CENTER);
 
   MYADDSPACER();
