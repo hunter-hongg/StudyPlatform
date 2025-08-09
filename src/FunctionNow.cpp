3ef779dc8b062aafbd4c2a1acdc714d4cedac876
localhost <localhost@localhost>
2025-08-09 13:51:23 +0800

    继续实现仙籍炼丹

--- src/FunctionNow.cpp
+++ src/FunctionNow.cpp
@@ -25,6 +25,24 @@ void MyFrame::xianji_liandan(WXBTNEVT&){
   });
   grid -> Add(btn1, FLAG_CENTER);
 
+  auto btn2 = new wxButton(panel,wxID_ANY,wxT("20仙籍+4银丹\n2金丹"));
+  MYSHOPBUTTON(btn2, MyOrange);
+  btn2 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&){
+    if(!XianJi::Reader.high(20)){
+      MYMESSAGE("仙籍不足");
+      return;
+    }
+    if(!Xian::Dan::Yin.high(4)){
+      MYMESSAGE("银丹不足");
+      return;
+    }
+    XianJi::Reader.minusnum(20);
+    Xian::Dan::Yin.minusnum(4);
+    Xian::Dan::Jin.addnum(2);
+    MYMESSAGE("炼制成功");
+  });
+  grid -> Add(btn2, FLAG_CENTER);
+
   vbox -> Add(grid, FLAG_CENTER);
 
   MYADDSPACER();
