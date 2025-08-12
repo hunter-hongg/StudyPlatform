2b31ae75f92f67f01fcbbfe532325721b8cca5a8
localhost <localhost@localhost>
2025-08-12 14:39:11 +0800

    继续实现金丹召鹤

--- src/FunctionNow.cpp
+++ src/FunctionNow.cpp
@@ -20,6 +20,20 @@ void MyFrame::xiandan_he_jindan(WXBTNEVT&){
   auto btn1 = new wxButton(panel, wxID_ANY, wxT("1金丹召鹤"));
   MYSHOPBUTTON(btn1, MyDarkOrange);
   btn1 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&){
+    if ( !Xian::Dan::Jin.canminus(1) ) {
+      MYMESSAGE("金丹不足");
+      return;
+    }
+    int luck = getrnd(0,100);
+    if(luck < 40) {
+      MYMESSAGE("召鹤成功，获得1000积分+50金币");
+      JiFenReader.addnum(1000);
+      JinBiReader.addnum(50);
+    } else {
+      MYMESSAGE("召鹤成功，获得900积分+40金币");
+      JiFenReader.addnum(900);
+      JinBiReader.addnum(40);
+    }
   });
   vbox -> Add(btn1, FLAG_CENTER);
 
