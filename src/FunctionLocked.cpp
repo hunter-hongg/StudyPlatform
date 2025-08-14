b9c62f658531cfe15422511d92f52b690344f42d
localhost <localhost@localhost>
2025-08-14 12:38:06 +0800

    调整函数

--- src/FunctionLocked.cpp
+++ src/FunctionLocked.cpp
@@ -2421,3 +2421,16 @@ void MyFrame::xiandan_he_shendan(WXBTNEVT&){
 
     MYLAST(&MyFrame::xiandan_he);
 }
+void MyFrame::xiandan_he(WXBTNEVT&){
+    MYINIT();
+  
+    MYTITLE("仙丹召鹤");
+
+    MYBUTTON(btn1, &MyFrame::xiandan_he_yindan, "银丹召鹤");
+    MYBUTTON(btn2, &MyFrame::xiandan_he_jindan, "金丹召鹤");
+    MYBUTTON(btn3, &MyFrame::xiandan_he_shendan, "神丹召鹤");
+  
+    MYADDSPACER();
+  
+    MYLAST(&MyFrame::xiandan);
+}
