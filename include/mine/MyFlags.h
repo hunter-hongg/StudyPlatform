c41de62a52613c5e0a985e7965dd1893aa0e3c63
localhost <localhost@localhost>
2025-08-14 12:51:54 +0800

    新建对齐方式

--- include/mine/MyFlags.h
+++ include/mine/MyFlags.h
@@ -4,4 +4,5 @@
 static const auto FLAG_CENTER=wxSizerFlags(0).Border(wxALL, 10).Align(wxALIGN_CENTER);
 static const auto FLAG_RIGHT=wxSizerFlags(0).Border(wxALL, 10).Align(wxALIGN_RIGHT);
 static const auto FLAG_LEFT=wxSizerFlags(0).Border(wxALL, 10).Align(wxALIGN_LEFT);
+static const auto FLAG_CENTER_NOBORDER=wxSizerFlags(0).Border(wxALL, 0).Align(wxALIGN_CENTER);
 
