41004ab22fdac529fa0c40146dca77cfcdf09f56
localhost <localhost@localhost>
2025-08-16 11:54:16 +0800

    调整函数

--- src/FunctionNow.cpp
+++ src/FunctionNow.cpp
@@ -12,31 +12,4 @@ void MyFrame::xianlu(WXBTNEVT&){
 
     MYLAST(&MyFrame::xianbi_square);
 }
-void MyFrame::xianlu_next(WXBTNEVT&){
-    MYINIT();
 
-    MYTITLE("仙禄升级");
-
-    auto getneed = [=](int dj){
-        return 1500 + dj * 100;
-    };
-
-    MYSHOWNSNCM(wxString(wxT("需要仙币："))+TOSTR(getneed(Xian::Lu::Ji.read_int())), btn1);
-    btn1 -> SetForegroundColour(MyDarkBlue);
-    
-    auto callback = [=](WXBTNEVT& evt){
-        int need = getneed(Xian::Lu::Ji.read_int());
-        if(!Files::XianBiReader.canminus(need)){
-            MYMESSAGE("仙币不足");
-            return;
-        }
-        Xian::Lu::Ji.addnum(1);
-        MYMESSAGE("升级成功");
-        this -> xianlu_next(evt);
-    };
-    MYBUTTONLAMBDA(btn2, callback, "升级仙禄");
-
-    MYADDSPACER();
-
-    MYLAST(&MyFrame::xianlu);
-}
