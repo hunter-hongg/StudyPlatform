135b190abd8e9d78383f7eab2d63f5e9b84f5596
localhost <localhost@localhost>
2025-08-17 08:28:29 +0800

    调整函数

--- src/FunctionNow.cpp
+++ src/FunctionNow.cpp
@@ -13,23 +13,4 @@ void MyFrame::xianlu(WXBTNEVT&){
 
     MYLAST(&MyFrame::xianbi_square);
 }
-void MyFrame::xianlu_get(WXBTNEVT&){
-    MYINIT();
 
-    MYTITLENS("仙禄领取");
-    MYSHOW(wxString(wxT("每日仙禄："))+TOSTR(lambda::xianlu_get_need())+wxT("仙币"));
-
-    auto callback = [=](WXBTNEVT&){
-        if(!Xian::Lu::CanLingQu.CheckTimes()){
-            MYMESSAGE("今日已领取");
-            return;
-        }
-        XianBiReader.addnum(lambda::xianlu_get_need());
-        MYMESSAGE("领取成功");
-    };
-    MYBUTTONLAMBDA(btn1, callback, "领取仙禄");
-
-    MYADDSPACER();
-
-    MYLAST(&MyFrame::xianlu);
-}
