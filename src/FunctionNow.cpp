d50ad0755d42a0489fa93e918556fa40bd3f80c8
localhost <localhost@localhost>
2025-08-17 08:32:34 +0800

    调整函数

--- src/FunctionNow.cpp
+++ src/FunctionNow.cpp
@@ -1,16 +1,3 @@
 #include <headers.hpp>
 
-void MyFrame::xianlu(WXBTNEVT&){
-    MYINIT();
-    
-    MYTITLENS("我的仙禄");
-    MYSHOW(wxString(wxT("仙禄等级："))+Xian::Lu::Ji.read_str());
-
-    MYBUTTON(btn1, &MyFrame::xianlu_next, "仙禄升级");
-    MYBUTTON(btn2, &MyFrame::xianlu_get, "领取仙禄");
-
-    MYADDSPACER();
-
-    MYLAST(&MyFrame::xianbi_square);
-}
 
--- src/FunctionSimple.cpp
+++ src/FunctionSimple.cpp
@@ -640,3 +640,17 @@ void MyFrame::xiandan(WXBTNEVT&){
 
     MYLAST(&MyFrame::xianbi_square);
 }
+void MyFrame::xianlu(WXBTNEVT&){
+    MYINIT();
+    
+    MYTITLENS("我的仙禄");
+    MYSHOW(wxString(wxT("仙禄等级："))+Xian::Lu::Ji.read_str());
+
+    MYBUTTON(btn1, &MyFrame::xianlu_next, "仙禄升级");
+    MYBUTTON(btn2, &MyFrame::xianlu_get, "领取仙禄");
+
+    MYADDSPACER();
+
+    MYLAST(&MyFrame::xianbi_square);
+}
+
