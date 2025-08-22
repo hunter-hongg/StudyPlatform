f9c5ba8a300345b3f26b0a3aad2e103fedd44fa1
localhost <localhost@localhost>
2025-08-22 08:57:16 +0800

    更新simple.hpp

--- include/simple.hpp
+++ include/simple.hpp
@@ -3,8 +3,10 @@
 #include <type.hpp>
 #include <mine/MyFonts.h>
 #include <mine/MyFlags.h>
+#include <mine/MyColour.h>
 #define fn auto
 namespace Simple{
+  // ------------------------------------------------------------------------------
   template<typename T>
   static fn Button(void(T::*fptr)(WXBTNEVT&), std::string value,
                    wxPanel* panel, wxBoxSizer* vbox, T* self) -> wxButton* {
@@ -14,24 +16,21 @@ namespace Simple{
     vbox -> Add(btn,FLAG_CENTER);
     return btn;
   }
+  static fn ShowButton(std::string showt, wxPanel* panel,
+                       wxBoxSizer* vbox) -> wxButton* {
+    auto show = new wxButton(panel,wxID_ANY,wxString::FromUTF8(showt)); 
+    show->SetFont(font15); 
+    show->SetForegroundColour(MyBlue); 
+    vbox->Add(show,FLAG_CENTER); 
+    vbox->AddStretchSpacer();
+    return show;
+  }
   static fn ShopButton(std::string value, wxPanel* panel,
                        wxColour colour ) -> wxButton* {
     auto btn = new wxButton(panel, wxID_ANY, wxString::FromUTF8(value));
     btn -> SetForegroundColour(colour);
     btn -> SetFont(font17);
     return btn;
-  }
-  template<typename T>
-  static fn Init(wxPanel* panel, T* self) -> wxBoxSizer* {
-    self->clean_panel(); 
-    return new wxBoxSizer(wxVERTICAL); 
-  }
-  static fn Title(std::string titlea, wxPanel* panel,
-                  wxBoxSizer* vbox) -> void {
-    auto title = new wxStaticText(panel,wxID_ANY,wxString::FromUTF8(titlea)); 
-    title -> SetFont(font25);
-    vbox -> Add(title,FLAG_CENTER); 
-    vbox -> AddStretchSpacer();
   }
   template<typename T>
   static fn BackButton(void(T::*fptr)(WXBTNEVT&), wxPanel* panel,
@@ -45,8 +44,29 @@ namespace Simple{
     panel -> SetSizer(vbox); 
     panel -> Layout();
   }
+  // ---------------------------------------------------------------------------------
+  template<typename T>
+  static fn Init(wxPanel* panel, T* self) -> wxBoxSizer* {
+    self->clean_panel(); 
+    return new wxBoxSizer(wxVERTICAL); 
+  }
+  static fn Title(std::string titlea, wxPanel* panel,
+                  wxBoxSizer* vbox) -> void {
+    auto title = new wxStaticText(panel,wxID_ANY,wxString::FromUTF8(titlea)); 
+    title -> SetFont(font25);
+    vbox -> Add(title,FLAG_CENTER); 
+    vbox -> AddStretchSpacer();
+  }
+  static fn TitleNoSpacer(std::string titlea, wxPanel* panel,
+                  wxBoxSizer* vbox) -> void {
+    auto title = new wxStaticText(panel,wxID_ANY,wxString::FromUTF8(titlea)); 
+    title -> SetFont(font25);
+    vbox -> Add(title,FLAG_CENTER);
+  }
+  // -------------------------------------------------------------------------------
   static fn Message(std::string msg) -> void {
     wxMessageBox(wxString::FromUTF8(msg));
   }
+  
 }
     
--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -5,6 +5,10 @@ void MyFrame::ancient_wuguan(WXBTNEVT&)
     auto vbox = Simple::Init(panel, this);
 
     Simple::Title("我的官位", panel, vbox);
+    Simple::ShowButton(
+        "我的类别：武官\n我的品级：",
+        panel, vbox
+    );
 
     Simple::Button(&MyFrame::ancient_wuguan_zhaomu, "征召士兵", panel, vbox, this);
     Simple::Button(&MyFrame::ancient_wuguan_show, "查看兵力", panel, vbox, this);
