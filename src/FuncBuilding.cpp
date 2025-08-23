87c32775144b8155d09f7efb55ca1adda7343d78
localhost <localhost@localhost>
2025-08-24 07:58:42 +0800

    开始实现古代武官-领兵出征

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -12,7 +12,15 @@ void MyFrame::ancient_wuguan(WXBTNEVT&)
 
     Simple::Button(&MyFrame::ancient_wuguan_zhaomu, "征召士兵", panel, vbox, this);
     Simple::Button(&MyFrame::ancient_wuguan_show, "查看兵力", panel, vbox, this);
+    Simple::Button(&MyFrame::ancient_wuguan_chuzheng, "领兵出征", panel, vbox, this);
 
     Simple::BackButton(&MyFrame::ancient_guan, panel, vbox, this);
 }
+void MyFrame::ancient_wuguan_chuzheng(WXBTNEVT&)
+{
+    auto vbox = Simple::Init(panel, this);
 
+    Simple::Title("领兵出征", panel, vbox);
+    
+    Simple::BackButton(&MyFrame::ancient_wuguan, panel, vbox, this);
+}
