2d5c59e480f38c585a4e772f6612aa1839110d9d
localhost <localhost@localhost>
2025-08-24 17:38:44 +0800

    更新FuncBuilding.cpp

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -18,6 +18,8 @@ void MyFrame::ancient_wuguan_1(WXBTNEVT&)
 {
     auto vbox = Simple::Init(panel, this);
 
+    Simple::Title("我的士兵", panel, vbox);
+
     Simple::Button(&MyFrame::ancient_wuguan_zhaomu, "征召士兵", panel, vbox, this);
     Simple::Button(&MyFrame::ancient_wuguan_show, "查看兵力", panel, vbox, this);
     Simple::Button(&MyFrame::ancient_wuguan_chuzheng, "领兵出征", panel, vbox, this);
