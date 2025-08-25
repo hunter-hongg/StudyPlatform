97bb9f69ca5302b5c3e1afd9150e3c5dbd603ef4
localhost <localhost@localhost>
2025-08-25 10:33:18 +0800

    调整函数

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -1,19 +1,5 @@
 #include <headers.hpp>
 
-void MyFrame::ancient_wuguan(WXBTNEVT&)
-{
-    auto vbox = Simple::Init(panel, this);
-
-    Simple::TitleNoSpacer("我的官位", panel, vbox);
-    Simple::ShowButton(
-        "我的类别：武官\n我的品级："+AncientVar::WuGuan::Pin::Get.ReadStr(),
-        panel, vbox
-    );
-
-    Simple::Button(&MyFrame::ancient_wuguan_1, "我的士兵", panel, vbox, this);
-
-    Simple::BackButton(&MyFrame::ancient_guan, panel, vbox, this);
-}
 void MyFrame::ancient_wuguan_1(WXBTNEVT&)
 {
     auto vbox = Simple::Init(panel, this);
