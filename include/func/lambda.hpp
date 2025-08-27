8a23b341112faf55fae3c31e6497db90b2f3c85c
localhost <localhost@localhost>
2025-08-28 07:27:05 +0800

    实现兵力计算函数

--- include/func/lambda.hpp
+++ include/func/lambda.hpp
@@ -15,4 +15,16 @@ static auto xianlu_get_need = [](){
         return 70 + 40 * 50; // 待扩展 
     }
 };
+static auto anc_wu_bingli_get = [](){
+    using namespace AncientVar::WuGuan;
+    return 
+        LiLiang::Xin.read_int() * BingLi::Xin.read_int() +    
+        LiLiang::PuTong.read_int() * BingLi::PuTong.read_int() +    
+        LiLiang::ChuJi.read_int() * BingLi::ChuJi.read_int() +    
+        LiLiang::ZhongJi.read_int() * BingLi::ZhongJi.read_int() +    
+        LiLiang::GaoJi.read_int() * BingLi::GaoJi.read_int() +    
+        LiLiang::JingRui.read_int() * BingLi::JingRui.read_int() +    
+        LiLiang::WangPai.read_int() * BingLi::WangPai.read_int() +    
+        LiLiang::ShenJi.read_int() * BingLi::ShenJi.read_int();    
+}
 }
