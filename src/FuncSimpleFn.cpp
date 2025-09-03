6f1437e140f5c418eaf0ad56a036151924d0606b
hunter-hongg <2843249813@qq.com>
2025-09-03 21:28:35 +0800

    调整函数

--- src/FuncSimpleFn.cpp
+++ src/FuncSimpleFn.cpp
@@ -14,3 +14,17 @@ void MyFrame::ancient_wuguan(WXBTNEVT&)
 
     Simple::BackButton(&MyFrame::ancient_guan, panel, vbox, this);
 }
+void MyFrame::ancient_wuguan_1(WXBTNEVT&)
+{
+    auto vbox = Simple::Init(panel, this);
+
+    Simple::Title("我的士兵", panel, vbox);
+
+    Simple::Button(&MyFrame::ancient_wuguan_zhaomu, "征召士兵", panel, vbox, this);
+    Simple::Button(&MyFrame::ancient_wuguan_show, "查看士兵", panel, vbox, this);
+    Simple::Button(&MyFrame::ancient_wuguan_bingli, "查看兵力", panel, vbox, this);
+    Simple::Button(&MyFrame::ancient_wuguan_chuzheng, "领兵出征", panel, vbox, this);
+
+    Simple::BackButton(&MyFrame::ancient_wuguan, panel, vbox, this);
+}
+
