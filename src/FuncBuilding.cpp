744d2712407c763b5bc2ba5eb07a214e5943b255
hunter-hongg <2843249813@qq.com>
2025-09-06 17:04:58 +0800

    继续开发

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -28,9 +28,11 @@ void MyFrame::ancient_wubingli_plus(WXBTNEVT&)
 
     auto grid = new wxGridSizer(3,3,4,4);
 
-    auto btn1 = new wxButton(panel, wxID_ANY, 
-        wxString::FromUTF8("新兵兵力 "+AncientVar::WuGuan::BingLi::Xin.read_str())
+    auto btn1 = Simple::ShopButton(
+        wxString::FromUTF8("新兵兵力 "+AncientVar::WuGuan::BingLi::Xin.read_str()), 
+        panel, MyRed  
     );
+    grid -> Add(btn1, FLAG_CENTER);
 
     vbox -> Add(grid, FLAG_CENTER);
 
