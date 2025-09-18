2a9b91d4af21108aa9a1950ea460f8ea06ca2c62
hunter-hongg <2843249813@qq.com>
2025-09-18 21:02:53 +0800

    继续开发

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -30,19 +30,4 @@ void MyFrame::ancient_wuguan_chuzheng(WXBTNEVT&)
     
     Simple::BackButton(&MyFrame::ancient_wuguan_1, panel, vbox, this);
 }   
-void MyFrame::ancient_wuguan_bingli(WXBTNEVT&)
-{ 
-    auto vbox = Simple::Init(panel, this);
 
-    Simple::TitleNoSpacer("查看兵力", panel, vbox);
-    Simple::ShowButton(
-        "我的兵力："+TOSTR(lambda::anc_wu_bingli_get()), panel, vbox);
-
-    Simple::Button(
-        &MyFrame::ancient_wubingli_get, "具体兵力", panel, vbox, this);
-    Simple::Button(
-        &MyFrame::ancient_wubingli_plus, "增强兵力", panel, vbox, this);
-    
-    Simple::BackButton(&MyFrame::ancient_wuguan_1, panel, vbox, this);
-}
- 
