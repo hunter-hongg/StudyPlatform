f48244d0d49a8c583b8e08e8f9b96560739044e6
hunter-hongg <2843249813@qq.com>
2025-10-11 21:59:26 +0800

    继续开发

--- include/gfunc.hpp
+++ include/gfunc.hpp
@@ -1,10 +1,26 @@
 #pragma once 
+#include <vector>
+#include <string>
 #include <func/simple.hpp>
 #include <clibs/ancient/ChuZheng.hpp>
 
 namespace gfunc {
-void WuGuanChuZhengFunc(WuGuanChuZheng::Choices cho){
+std::string WuGuanChuZhengTrans(WuGuanChuZheng::Choices t){
     using namespace WuGuanChuZheng;
-
+    switch(t) {
+        case Choices::JinGong: return "进攻敌方"; break; 
+        case Choices::XiuZheng: return "修整兵马"; break; 
+        case Choices::FangShou: return "防御敌军"; break;
+        default: return "趁其不备"; break; 
+    }
+}
+std::vector<int> WuGuanChuZhengFunc(WuGuanChuZheng::Choices cho, int bingls, int binglo){
+    using namespace WuGuanChuZheng;
+    Choices ComputerChoice = GetChoice(binglo, bingls);
+    Choices UserChoice = cho;
+    Simple::Message(
+        "你的选择: "+WuGuanChuZhengTrans(UserChoice)+"\n"+
+        "电脑选择: "+WuGuanChuZhengTrans(ComputerChoice)
+    );
 }
 }
--- include/headers.hpp
+++ include/headers.hpp
@@ -17,6 +17,7 @@
 #include <type.hpp> // 定义类型别名等
 #include <macro.hpp> // 定义宏等
 #include <global.hpp> // 定义全局变量
+#include <gfunc.hpp> // 定义全局函数
 #include <mine/MyFonts.h> // 定义字体等
 #include <mine/MyFlags.h> // 定义对齐方式等
 #include <mine/MyColour.h> // 定义颜色等
