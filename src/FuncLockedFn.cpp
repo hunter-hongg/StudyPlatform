eca3ccfc4632d65d9bd69f146d00cc77ce962501
hunter-hongg <2843249813@qq.com>
2025-10-18 11:08:18 +0800

    修理Bug

--- src/FuncLockedFn.cpp
+++ src/FuncLockedFn.cpp
@@ -286,6 +286,7 @@ void MyFrame::ancient_wuguan_chuzheng(WXBTNEVT&)
         "我的兵力："+TOSTR(lambda::anc_wu_bingli_get()), panel, vbox);
 
     auto mybingl = lambda::anc_wu_bingli_get();
+    Simple::Message(TOSTR(mybingl))
     int add_or_sub_max = mybingl / 20;
     if ( add_or_sub_max <= 1 ) {
         add_or_sub_max = 2;
--- src/FuncSimpleFn.cpp
+++ src/FuncSimpleFn.cpp
@@ -48,14 +48,18 @@ void MyFrame::ancient_wuguan_chuzheng_1(WXBTNEVT&){
     using Global::AncientWuGuanChuZheng::bingl_sf;
     using Global::AncientWuGuanChuZhengReal::bingl_ot_now;
     using Global::AncientWuGuanChuZhengReal::bingl_sf_now;
-    if ( (bingl_ot_now == -1) && (bingl_sf_now == -1) ) {
-        bingl_ot_now = bingl_ot;
-        bingl_sf_now = bingl_sf;
-    }
 
     if ( ( bingl_sf < 0 ) || ( bingl_ot < 0 ) ) {
         Simple::MessageErr("运行错误");
-        throw cgstdErr::RuntimeErr();
+        Simple::MessageErr(TOSTR(bingl_sf));
+       // throw cgstdErr::RuntimeErr();
+        bingl_ot_now = bingl_ot; 
+        bingl_sf_now = bingl_sf;
+    }
+
+    if ( (bingl_ot_now <= -1) && (bingl_sf_now <= -1) ) {
+        bingl_ot_now = bingl_ot;
+        bingl_sf_now = bingl_sf;
     }
         
     auto vbox = Simple::Init(panel, this);
