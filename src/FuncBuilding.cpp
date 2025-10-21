2007330e92b1dfe08bfb347ad38cfe6d558d429f
hunter-hongg <2843249813@qq.com>
2025-10-21 21:13:41 +0800

    更新提示内容

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -1,8 +1,16 @@
 #include <headers.hpp>
 
 void MyFrame::acz_win(){
-    Simple::Message("测试消息：胜利");
+    Simple::Message("恭喜你，这次出征大获全胜");
+    Simple::Message("胜不骄，败不馁");
+    Simple::Message(
+        "此战结果：\n"
+    );
 }
 void MyFrame::acz_lose(){
-    Simple::Message("测试消息：失败");
+    Simple::Message("很遗憾，这次出征以失利告终");
+    Simple::Message("胜败乃兵家常事");
+    Simple::Message(
+        "此战结果：\n"
+    );
 }
