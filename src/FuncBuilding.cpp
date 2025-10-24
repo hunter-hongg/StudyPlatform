8787ac75f1b2ffb6f16925da6008cafdc3295b33
hunter-hongg <2843249813@qq.com>
2025-10-24 21:28:58 +0800

    调整函数

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -1,23 +1,5 @@
 #include <headers.hpp>
 
-void MyFrame::acz_win(){
-    int p1 = getrnd(20, 40); // 中级兵
-    int p2 = getrnd(5, 10); // 高级兵
-    int p3 = getrnd(0, 3); // 神级兵
-    Simple::Message("恭喜你，这次出征大获全胜");
-    Simple::Message("胜不骄，败不馁");
-    Simple::Message(
-        "此战结果：\n"
-        "黄金+20两\n"
-        "中级兵+"+TOSTR(p1)+"\n"
-        "高级兵+"+TOSTR(p2)+"\n"
-        "神级兵+"+TOSTR(p3)+"\n"
-    );
-    AncientVar::HuangJinReader.addnum(20);
-    AncientVar::WuGuan::LiLiang::ZhongJi.addnum(p1);
-    AncientVar::WuGuan::LiLiang::GaoJi.addnum(p2);
-    AncientVar::WuGuan::LiLiang::ShenJi.addnum(p3);
-}
 void MyFrame::acz_lose(){
     int p1 = getrnd(20, 100); // 中级兵
     int p2 = getrnd(10, 40); // 高级兵
