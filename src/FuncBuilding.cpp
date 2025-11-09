eb282fee160931a68c7dfbb92a4f544b7a3ed5ce
hunter-hongg <2843249813@qq.com>
2025-11-09 20:16:46 +0800

    微调

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -8,11 +8,17 @@ void MyFrame::ancient_shop_bianli(WXBTNEVT&) {
 
     Simple::Title("便利店铺", panel, vbox);
 
+    wxGridSizer* grid = new wxGridSizer(3, 3, 4, 4);
+
+    vbox -> Add(grid);
+
     Simple::BackButton(&MyFrame::ancient_shopa, panel, vbox, this);
 }
 void MyFrame::ancient_shop_bianli_baiyin(WXBTNEVT&) {
     wxBoxSizer* vbox = Simple::Init(panel, this);
 
     Simple::Title("兑换白银", panel, vbox);
+
+    Simple::BackButton(&MyFrame::ancient_shop_bianli_baiyin, panel, vbox, this);
 }
 
