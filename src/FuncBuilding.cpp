ebe6c60bc169a99b4685c1a453a8adc7615387d0
hunter-hongg <2843249813@qq.com>
2025-11-09 11:22:09 +0800

    新建古代店铺

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -1,3 +1,11 @@
-#include "var.hpp"
-#include <headers.hpp>
+#include "func/simple.hpp"
+#include "type.hpp"
+#include "function.hpp"
 
+void MyFrame::ancient_shop_bianli(WXBTNEVT&) {
+    auto vbox = Simple::Init(panel, this);
+
+    Simple::Title("便利店铺", panel, vbox);
+
+    Simple::BackButton(&MyFrame::ancient_shopa, panel, vbox, this);
+}
