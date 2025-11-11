48949916519ff02fa31b7f8cd49d8aa9f08db4a9
hunter-hongg <2843249813@qq.com>
2025-11-11 21:29:02 +0800

    继续开发

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -1,10 +1,14 @@
+#include "func/simple.hpp"
 #include "headers.hpp"
+#include "macro.hpp"
 #include "mine/MyFlags.h"
 #include "mine/MyFonts.h"
 #include "type.hpp"
+#include <string>
 #include <wx/event.h>
 #include <wx/gtk/button.h>
 #include <wx/gtk/stattext.h>
+#include <wx/msgdlg.h>
 #include <wx/wx.h>
 
 void MyFrame::ancient_shop_bianli(WXBTNEVT&) {
@@ -38,7 +42,17 @@ void MyFrame::ancient_shop_bianli_baiyin(WXBTNEVT&) {
     auto Submit = new wxButton(panel, wxID_ANY, wxT("兑换"));
     Submit -> SetFont(font17);
     Submit -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&) {
-
+        std::string ans = (ReadTo -> GetValue()).ToStdString();
+        int t = -1;
+        try {
+            t = std::stoi(ans);
+        } catch(...) {
+            t = -1;
+        }
+        if(t <= 0) {
+            Simple::MessageErr("输入格式错误");
+            return;
+        }
     });
     vbox -> Add(Submit, FLAG_LEFT);
 
