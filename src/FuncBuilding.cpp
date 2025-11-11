42fe6f70a889f060c13f9488a8e609f93f7c5c1d
hunter-hongg <2843249813@qq.com>
2025-11-11 21:22:59 +0800

    继续开发

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -1,7 +1,9 @@
 #include "headers.hpp"
 #include "mine/MyFlags.h"
 #include "mine/MyFonts.h"
+#include "type.hpp"
 #include <wx/event.h>
+#include <wx/gtk/button.h>
 #include <wx/gtk/stattext.h>
 #include <wx/wx.h>
 
@@ -30,7 +32,15 @@ void MyFrame::ancient_shop_bianli_baiyin(WXBTNEVT&) {
     vbox -> Add(label, FLAG_LEFT);
 
     auto ReadTo = new wxTextCtrl(panel, wxID_ANY, wxT("请输入..."));
+    ReadTo -> SetFont(font17);
     vbox -> Add(ReadTo, FLAG_LEFT);
+
+    auto Submit = new wxButton(panel, wxID_ANY, wxT("兑换"));
+    Submit -> SetFont(font17);
+    Submit -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&) {
+
+    });
+    vbox -> Add(Submit, FLAG_CENTER);
 
     Simple::BackButton(&MyFrame::ancient_shop_bianli, panel, vbox, this);
 }
