dc9582b6f47b57492b098cb8ee1b84226d88a90a
hunter-hongg <2843249813@qq.com>
2025-11-23 10:50:03 +0800

    继续开发便利商店

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -36,13 +36,9 @@ void MyFrame::ancient_shop_bianli_book(WXBTNEVT&) {
     btn2 -> Bind(wxEVT_BUTTON, &MyFrame::ancient_shop_bianli_book_zhengui, this);
     grid -> Add(btn2, FLAG_CENTER);
 
-    // auto btn2 = Simple::ShopButton("兑换黄金", panel, MyOrange);
-    // btn2 -> Bind(wxEVT_BUTTON, &MyFrame::ancient_shop_bianli_huangjin, this);
-    // grid -> Add(btn2, FLAG_CENTER);
-    //
-    // auto btn3 = Simple::ShopButton("兑换铜钱", panel, MyOrange);
-    // btn3 -> Bind(wxEVT_BUTTON, &MyFrame::ancient_shop_bianli_tongbi, this);
-    // grid -> Add(btn3, FLAG_CENTER);
+    auto btn3 = Simple::ShopButton("购买典藏书籍", panel, MyOrange);
+    btn3 -> Bind(wxEVT_BUTTON, &MyFrame::ancient_shop_bianli_diancang, this);
+    grid -> Add(btn3, FLAG_CENTER);
 
     vbox -> Add(grid, FLAG_CENTER);
 
@@ -86,6 +82,50 @@ void MyFrame::ancient_shop_bianli_book_zhengui(WXBTNEVT&) {
             BookShelfFiles_WriteLevel2(BookShelf::Reader, t);
             Simple::Message("购买成功");
             CLogger_log(Logfile, CLogger_DEBUG, "便利商店=>书籍商店=>珍贵书籍: 购买成功");
+        }
+    });
+    vbox -> Add(Submit, FLAG_LEFT);
+
+    Simple::BackButton(&MyFrame::ancient_shop_bianli_book, panel, vbox, this);
+}
+void MyFrame::ancient_shop_bianli_book_diancang(WXBTNEVT&) {
+    CLogger_log(Logfile, CLogger_DEBUG, "便利商店=>书籍商店=>典藏书籍: 正常启动");
+
+    wxBoxSizer* vbox = Simple::Init(panel, this);
+
+    Simple::Title("购买典藏书籍", panel, vbox);
+
+    auto label = new wxStaticText(panel, wxID_ANY, wxT("请输入需要购买多少典藏书籍"));
+    label->SetFont(font17);
+    vbox -> Add(label, FLAG_LEFT);
+
+    auto ReadTo = new wxTextCtrl(panel, wxID_ANY, wxT("请输入..."));
+    ReadTo -> SetFont(font15);
+    vbox -> Add(ReadTo, FLAG_LEFT);
+
+    auto Submit = new wxButton(panel, wxID_ANY, wxT("购买"));
+    Submit -> SetFont(font17);
+    Submit -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&) {
+        std::string ans = (ReadTo -> GetValue()).ToStdString();
+        int t = -1;
+        try {
+            t = std::stoi(ans);
+        } catch(...) {
+            t = -1;
+        }
+        if(t <= 0) {
+            Simple::MessageErr("输入格式错误");
+            CLogger_log(Logfile, CLogger_WARNING, "便利商店=>书籍商店=>典藏书籍: 输入格式错误");
+            return;
+        }
+        int need = t*50;
+        if(!AncientVar::BaiYinReader.canminus(need)) {
+            Simple::Message("白银不足");
+            return;
+        } else {
+            BookShelfFiles_WriteLevel3(BookShelf::Reader, t);
+            Simple::Message("购买成功");
+            CLogger_log(Logfile, CLogger_DEBUG, "便利商店=>书籍商店=>典藏书籍: 购买成功");
         }
     });
     vbox -> Add(Submit, FLAG_LEFT);
