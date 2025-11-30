e9376dfa95dcedcdd045aa422dcbafc0108edc3c
hunter-hongg <2843249813@qq.com>
2025-11-30 08:11:13 +0800

    开发基本界面

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -18,9 +18,16 @@ void MyFrame::bank_juan_use(WXBTNEVT&) {
     Simple::ShowButton(sfsa.str(), panel, vbox);
 
     auto btn1 = Simple::Button("积分奖励", panel, vbox);
-    btn1 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&){
+    btn1 -> Bind(wxEVT_BUTTON, [=](WXBTNEVT&) {
         GlobalSignal.BankJuanUseJiFen.emit();
     });
 
     Simple::BackButton(&MyFrame::bank_juan, panel, vbox, this);
+}
+void MyFrame::bank_juan_use_jifen(WXBTNEVT&) {
+    auto vbox = Simple::Init(panel, this);
+
+    Simple::Title("积分奖励", panel, vbox);
+
+    Simple::BackButton(&MyFrame::bank_juan_use, panel, vbox, this);
 }
