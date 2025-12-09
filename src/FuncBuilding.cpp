7a29cbf4bd757709ae2fb9f91b1fdbb0c090026c
hunter-hongg <2843249813@qq.com>
2025-12-09 20:08:06 +0800

    更新文件

--- src/FuncBuilding.cpp
+++ src/FuncBuilding.cpp
@@ -6,21 +6,3 @@
 #include <wx/gtk/stattext.h>
 #include <wx/wx.h>
 
-void MyFrame::ancient_bookstore_chaolu(WXBTNEVT&) {
-    auto vbox = Simple::Init(panel, this);
-
-    Simple::Title("抄录书籍", panel, vbox);
-    // Simple::ShowButton("", panel, vbox);
-
-    Simple::Button(&MyFrame::ancient_bookstore_chaolu_start, "开始抄录", panel, vbox, this);
-
-    Simple::BackButton(&MyFrame::ancient_bookstore, panel, vbox, this);
-}
-void MyFrame::ancient_bookstore_chaolu_start(WXBTNEVT&) {
-    auto vbox = Simple::Init(panel, this);
-
-    Simple::Title("开始抄录", panel, vbox);
-
-    Simple::BackButton(&MyFrame::ancient_bookstore_chaolu, panel, vbox, this);
-}
-
