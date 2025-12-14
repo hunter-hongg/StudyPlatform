e8c2d2fbdc4972088bbd8214755c02e2c0555bd6
hunter-hongg <2843249813@qq.com>
2025-12-14 16:21:34 +0800

    完成通用货币与积分兑换

--- src/pages/tongjifenpage.go
+++ src/pages/tongjifenpage.go
@@ -4,6 +4,7 @@ import (
 	"StudyPlatform/src/interfaces"
 	"StudyPlatform/src/pkg/global"
 	"StudyPlatform/src/pkg/simple"
+	"strconv"
 
 	"image/color"
 
@@ -44,6 +45,21 @@ func (p *TongJifenPage) GetContent() fyne.CanvasObject {
 	tishi.TextSize = 20
 	inputTong := widget.NewEntry()
 	inputTong.Text = "请输入..."
+	btnOk := widget.NewButton("兑换", func(){
+		read := inputTong.Text
+		readi, err := strconv.Atoi(read)
+		if err != nil {
+			simple.DialogError(err, global.Main_Window)
+			return
+		}
+		if res, err := global.File_JiFenReader.CanMinus(readi * 1000);(!res) || (err != nil) {
+			simple.DialogInfo("取出积分时出错", global.Main_Window)
+			return
+		}
+		global.File_TongYongReader.AddNum(readi)
+		simple.DialogInfo("兑换成功", global.Main_Window)
+		p.router(interfaces.PageID_TongJifenPage)
+	})
 
 	vbox := container.NewVBox(
 		simple.HorizonCenter(title),
@@ -53,6 +69,7 @@ func (p *TongJifenPage) GetContent() fyne.CanvasObject {
 		simple.Spacer(130),
 		simple.HorizonLeft(tishi),
 		simple.HorizonLeftHalf(inputTong),
+		simple.HorizonLeft(btnOk),
 		simple.Spacer(100),
 		simple.HorizonRight(btnBack),
 		layout.NewSpacer(),
