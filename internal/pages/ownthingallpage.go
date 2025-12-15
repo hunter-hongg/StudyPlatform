4a0e7517e50eaea4a6d7cf938738d91877b541c4
hunter-hongg <2843249813@qq.com>
2025-12-15 20:55:44 +0800

    添加纯Go语言Logger

--- internal/pages/ownthingallpage.go
+++ internal/pages/ownthingallpage.go
@@ -24,6 +24,8 @@ func NewOwnThingAllPage() *OwnThingAllPage {
 }
 
 func (p *OwnThingAllPage) GetContent() fyne.CanvasObject {
+	global.Logger.Debug("个人主页=>所有物品=>物品查看: 正常启动")
+
 	title := canvas.NewText("物品查看", color.Black)
 	title.TextSize = 27
 
