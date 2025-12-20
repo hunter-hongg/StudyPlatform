5f7d9daa8c92b52a0e6f2e52d26b635457a2fdfa
hunter-hongg <2843249813@qq.com>
2025-12-20 09:49:56 +0800

    完成宝石获取

--- internal/pages/baoshishoppage.go
+++ internal/pages/baoshishoppage.go
@@ -23,6 +23,18 @@ func NewOwnThingBaoshiShopPage() *OwnThingBaoshiShopPage {
 }
 
 func (p *OwnThingBaoshiShopPage) GetContent() fyne.CanvasObject {
+	global.Logger.Debug("正常启动")
+
+	dealBaoshiWithJifen := func(jifen, baoshi int) {
+		if res, err := global.File_JiFenReader.CanMinus(jifen); (!res) || (err != nil) {
+			simple.DialogInfo("取出积分失败",global.Main_Window)
+			return
+		}
+		global.File_BaoshiReader.AddNum(baoshi)
+		simple.DialogInfo("兑换成功", global.Main_Window)
+		p.router(interfaces.PageID_OwnThingBaoshiShopPage)
+	}
+
 	title := canvas.NewText("获取宝石", color.Black)
 	title.TextSize = 27
 
@@ -31,16 +43,44 @@ func (p *OwnThingBaoshiShopPage) GetContent() fyne.CanvasObject {
 	simple.Blue)
 	showbaoshi.TextSize = 20
 
+	showjifen := canvas.NewText(
+		"积分: "+global.File_JiFenReader.ReadStrSafe(),
+	simple.Blue)
+	showjifen.TextSize = 20
+
 	btnBack := widget.NewButton("返回", func(){
 		p.router(interfaces.PageID_OwnThingBaoshiPage)
+	})
+
+	btn1 := widget.NewButton("200积分 => 5宝石", func() {
+		dealBaoshiWithJifen(200, 5)
+	})
+
+	btn2 := widget.NewButton("400积分 => 11宝石", func() {
+		dealBaoshiWithJifen(400, 11)
+	})
+
+	btn3 := widget.NewButton("600积分 => 18宝石", func() {
+		dealBaoshiWithJifen(600, 18)
+	})
+
+	btn4 := widget.NewButton("800积分 => 25宝石", func() {
+		dealBaoshiWithJifen(800, 25)
+	})
+
+	btn5 := widget.NewButton("1000积分 => 35宝石", func() {
+		dealBaoshiWithJifen(1000, 35)
 	})
 
 	vbox := container.NewVBox(
 		simple.HorizonCenter(title),
 		simple.Spacer(20),
 		simple.HorizonCenter(showbaoshi),
-		simple.Spacer(150),
+		simple.HorizonCenter(showjifen),
+		simple.Spacer(120),
 		container.NewGridWithColumns(3, 
+			btn1, btn2, btn3, 
+			btn4, btn5, 
 		),
 		simple.Spacer(100),
 		simple.HorizonRight(btnBack),
