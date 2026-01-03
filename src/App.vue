8948c21c53a86147c41c9ca2eb0b63eacd79be3d
hunter-hongg <2843249813@qq.com>
2026-01-03 17:50:56 +0800

    严格实现get_user后端

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
-
-const greetMsg = ref("");
-const name = ref("");
+// import { ref } from "vue";
+// import { invoke } from "@tauri-apps/api/core";
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
+  // greetMsg.value = await invoke("greet", { name: name.value });
 }
 </script>
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <h1>欢迎</h1>
 
     <div class="row">
       <a href="https://vite.dev" target="_blank">
@@ -26,13 +23,7 @@ async function greet() {
         <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
       </a>
     </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
   </main>
 </template>
 
