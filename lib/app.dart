bf28231cfd6d85cb77b3de04fa0e9f49feb41a38
hunter-hongg <2843249813@qq.com>
2026-01-09 19:24:07 +0800

    完成网格布局

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/home_page.dart';
+import 'pages/own_page.dart'; // 导入其他页面
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope( // Riverpod全局容器
+    return ProviderScope(
       child: MaterialApp(
         title: '学习平台',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
         home: HomePage(),
+        routes: {
+          '/second': (context) => OwnPage(),
+        },
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,39 +4,78 @@ import 'package:study_platform/logic/env_mod.dart';
 import 'package:study_platform/vars/files.dart';
 import 'package:study_platform/vars/styles.dart';
 
-// Riverpod状态提供者（全局可访问）
 final counterProvider = StateProvider<int>((ref) => 0);
 final user = EnvMod.getUser();
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
-  // ConsumerWidget用于访问状态
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final jifenStr = Files.jiFenReader().readStrSafeSync();
+    
+    // 左边列的项目
+    final List<Widget> leftColumnItems = [
+      ElevatedButton(
+        onPressed: () {},
+        style: Styles.buttonSimpleStyle(),
+        child: const Text("个人主页"),
+      ),
+      ElevatedButton(
+        onPressed: () {},
+        style: Styles.buttonSimpleStyle(),
+        child: const Text("计算广场"),
+      ),
+    ];
+    
+    // 右边列的项目
+    final List<Widget> rightColumnItems = [
+      ElevatedButton(
+        onPressed: () {},
+        style: Styles.buttonSimpleStyle(),
+        child: const Text("神话传说"),
+      ),
+      ElevatedButton(
+        onPressed: () {},
+        style: Styles.buttonSimpleStyle(),
+        child: const Text("古代广场"),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('欢迎$user来到学习平台'),
         centerTitle: true,
         titleTextStyle: Styles.titleStyle(),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 5,),
-            Text('积分: $jifenStr',
-                style: Styles.showstrStyle())
-            //   const SizedBox(height: 20),
-            //   ElevatedButton(
-            //     onPressed: () async {
-            //       final current = ref.read(counterProvider);
-            //       final newCount = await CounterService.incrementFromBackend(current);
-            //       ref.read(counterProvider.notifier).state = newCount;
-            //     },
-            //     child: const Text('点击+1'),
-            //   ),
-          ],
-        ),
+      body: Column(
+        children: [
+          SizedBox(height: 10),
+          Text('积分: $jifenStr', style: Styles.showstrStyle()),
+          SizedBox(height: 80),
+          Center(
+            child: Row(
+              spacing: 20,
+            )
+          ),
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 20,
+            children: [
+              leftColumnItems[0], 
+              rightColumnItems[0],
+            ],
+          ),
+          SizedBox(height: 20,),
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 20,
+            children: [
+              leftColumnItems[1],
+              rightColumnItems[1],
+            ],
+          )
+        ],
       ),
     );
   }
