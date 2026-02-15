a23117bd5c6c733c7203a65f8b94e6f54c5322a4
hunter-hongg <2843249813@qq.com>
2026-02-15 14:02:32 +0800

    跟进文档

--- lib/logic/xianhe.dart
+++ lib/logic/xianhe.dart
@@ -6,18 +6,6 @@ import 'package:study_platform/vars/files.dart';
 import 'package:study_platform/vars/simple.dart';
 
 class XianHe {
-/*
-	deal := func(fpo *util.FilePassword, jifadd int, jinadd int) {
-		if res, err := fpo.CanMinus(1); (!res) || (err!=nil){
-			simple.DialogInfo("仙丹不足", global.Main_Window)
-			return
-		}
-		rndp := cgo.GetRnd(-50, 100)
-		global.File_JiFenReader.AddNum(jifadd+rndp)
-		global.File_JinBiReader.AddNum(jinadd+rndp)
-		simple.DialogInfo("召鹤成功 获得"+strconv.Itoa(jifadd+rndp)+"积分和"+strconv.Itoa(jinadd+rndp)+"金币", global.Main_Window)
-	}
-  */
   static void zhao(BuildContext context, FilePassword xd, int jifadd, int jinadd) {
     Trade.tradeFFunc(context, xd, '仙丹', 1, () {
       var rnd = RandomGet.getIntClose(-50, 110);
